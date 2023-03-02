2295d6fd14606c4f6a66d2c1e1c4e0c46bff9e5d
darshanjag <itsdarshanjagtap@gmail.com>
2023-03-02 18:44:03 +0530

    my code

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -1,66 +1,74 @@
 package com.driver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
+import java.util.List;
 @Repository
 public class StudentRepository {
-    HashMap<String, Student> studentDB = new HashMap<>();//Database for student
-    HashMap<String, Teacher> teacherDB = new HashMap<>();//Database for teacher
-    HashMap<String, List<String>> student_teacherDB = new HashMap<>(); //Database for student Teacher pair
+    private HashMap<String,Student> studentDb = new HashMap<>();
+    private HashMap<String,Teacher> teacherDb = new HashMap<>();
+
+    private HashMap<String,List<String>> teacherStudentDb = new HashMap<>();
 
     public void addStudent(Student student){
-        studentDB.put(student.getName(), student);
+        String name = student.getName();
+        studentDb.put(name,student);
     }
     public void addTeacher(Teacher teacher){
-        teacherDB.put(teacher.getName(), teacher);
+        String name = teacher.getName();
+        teacherDb.put(name,teacher);
+    }
+
+    public Student getStudentByName(String name){
+        return studentDb.get(name);
+    }
+
+    public Teacher getTeacherByName(String name){
+        return teacherDb.get(name);
     }
     public void addStudentTeacherPair(String student, String teacher){
-        if (student_teacherDB.containsKey(teacher)){
-            List<String> teacher_list = student_teacherDB.get(teacher);
-            teacher_list.add(student);
-            student_teacherDB.put(teacher,teacher_list);
-        }else {
-            List<String> teacher_list = new ArrayList<>();
-            teacher_list.add(student);
-            student_teacherDB.put(teacher,teacher_list);
+        List<String> list = new ArrayList<>();
+        if(teacherStudentDb.containsKey(teacher)){
+            list =teacherStudentDb.get(teacher);
+            list.add(student);
         }
-    }
-    public Student getStudentByName(String name){
-        return studentDB.get(name);
-    }
-    public Teacher getTeacherByName(String name){
-        return teacherDB.get(name);
+        teacherStudentDb.put(teacher,list);
+        teacherDb.get(teacher).setNumberOfStudents(list.size());
+
     }
     public List<String> getStudentsByTeacherName(String teacher){
-        return student_teacherDB.get(teacher);
+        return teacherStudentDb.get(teacher);
+
     }
     public List<String> getAllStudents(){
         List<String> list = new ArrayList<>();
-        for(String s: studentDB.keySet()){
-            list.add(s);
+        for(String student:studentDb.keySet()){
+            list.add(student);
         }
         return list;
     }
-    public void deleteTeacherByName(String teacher){
-        List<String> student_list = student_teacherDB.get(teacher);
-        for (String s: student_list){
-            studentDB.remove(s);
+    public void deleteTeacherByName(String name){
+        List<String> students = new ArrayList<>();
+        if(teacherStudentDb.containsKey(name)){
+            students=teacherStudentDb.get(name);
+            for(String student: students){
+                studentDb.remove(student);
+            }
+            teacherStudentDb.remove(name);
+            teacherDb.remove(name);
         }
-        teacherDB.remove(teacher);
-        student_teacherDB.remove(teacher);
     }
     public void deleteAllTeachers(){
-        for (List<String> student_list: student_teacherDB.values()){
+        for (List<String> student_list: teacherStudentDb.values()){
             for (String s: student_list){
-                studentDB.remove(s);
+                studentDb.remove(s);
             }
         }
-        teacherDB.clear();
-        student_teacherDB.clear();
+        teacherDb.clear();
+
     }
+
 }
